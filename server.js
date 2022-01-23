c54e490aa0189ddd459f44fe3a6a799c933ac5b5
hy <yang.hrn@gmail.com>
2022-01-23 14:09:16 +0000

    landing page to note page

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
-//import express - install package
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const util = require("util");
+const uuid = require("./helpers/uuid");
 
-//import apiRoutes
-//import html folder
+// const router = express.Router();
+// const apiRoutes = require("./routes/apiRoutes");
+// const htmlRoutes = require("./routes/htmlRoutes");
+// router.use("./routes/apiRoutes.js");
 
-//intialise the app and create a port
-// app = express()
-// const PORT = process.emv.PORT || 3001
+const PORT = process.env.PORT || 3001;
+exp = express();
+
+exp.use(express.json());
+exp.use(express.urlencoded({ extended: true }));
+exp.use(express.static("public"));
 
 //middleware
-//app.use
+exp.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
 
-//start our serve
-
-// app.listen
+//start our server
+exp.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
